40999f40a7626d104755f087527746fdfab07d5d
PJ Fancher <peejfancher@gmail.com>
2022-12-21 12:55:39 -0500

    main@unknown: Started scaper

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 const cheerio = require('cheerio');
-const htmlparser2 = require('htmlparser2');
 const fs = require('fs');
 
+let shows = [];
+let show_count = 0;
+let date;
+let bands;
+let venue;
 const $ = cheerio.load(fs.readFileSync('data/2002.mhtml'));
-const shows = $('.showlistings', 'body');
-console.log(shows.html())
+const rows = $('.showlistings tr td', 'body');
+
+rows.each((i, el) => {
+  const td = $(el);
+
+  if( td.hasClass('maindate') ) {
+    date = td.text();
+  }
+
+  if( td.hasClass('venue') ) {
+    venue = td.text().trim();
+
+  }
+
+  if( td.hasClass('bandlist')) {
+    bands = [];
+
+    $('b', td).each((i, el) => {
+      const b = $(el);
+      bands[i] = b.text();
+    })
+
+    shows[show_count] = {
+      date: date,
+      venue: venue,
+      bands: bands
+    }
+    show_count++;
+  }
+
+});
+
+console.log(shows)
