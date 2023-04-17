b681938d82e22c15bbee9b0358c846957f143082
PJ Fancher <peejfancher@gmail.com>
2023-04-17 13:18:37 -0400

    master@unknown: new export

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const cheerio = require('cheerio');
-const fs = require('fs');
+const cheerio = require("cheerio");
+const fs = require("fs");
 
 let shows = [];
 let show_count = 0;
@@ -7,28 +7,28 @@ let date;
 let bands;
 let venue;
 
-const year = 2008;
-const filename = year + '-Oct';
-const $ = cheerio.load(fs.readFileSync('data/HTML/' + filename + '.html'));
-const rows = $('table tr', 'body');
+const year = 2005;
+const filename = year;
+const $ = cheerio.load(fs.readFileSync("data/HTML/" + filename + ".html"));
+const rows = $("table tr", "body");
 
 rows.each((i, row) => {
-  const tds = $(row).find('td');
-  // console.log(tds.length)
-  if(tds.length < 1) return; //skip the header row
-  date  = $(tds[2]).text().trim() + ', ' + year;
-  venue = $(tds[3]).text().trim();
-  bands = $(tds[5]).text().split(',');
-  bands = bands.map(band => band.trim()); //trim the leading spaces
+  const tds = $(row).find("td");
+  if (tds.length < 1) return; //skip the header row
+  date = $(tds[1]).text().trim() + ", " + year;
+  venue = $(tds[2]).text().trim();
+  bands = $(tds[4]).text().split(",");
+  bands = bands.map((band) => band.trim()); //trim the leading spaces
+
+  if (date == ", " + year) return; //skip the header row
 
   shows[show_count] = {
-    date  : date,
-    venue : venue,
-    bands : bands
-  }
+    date: date,
+    venue: venue,
+    bands: bands,
+  };
   show_count++;
 });
 
-
-console.log(JSON.stringify(shows))
-fs.writeFileSync('data/JSON/' + filename + '.json', JSON.stringify(shows));
+console.log(JSON.stringify(shows));
+fs.writeFileSync("data/JSON/" + filename + ".json", JSON.stringify(shows));
